f606af8ef3149849397cf6902274ec702b8745d3
Fred <email@Fred.com>
2020-11-19 17:25:58 -0500

    boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $1
+done
+
+liste=ls #liste serait egal a "ls"
+liste=`ls /home` #liste est egal au resultat de la commande ls
+for item in $liste
+do
+	echo $item
+done
+
+touche=o
+
+while test $touche == o
+do
+	read -p "entrer o pour infini" touche
+done
+
+
