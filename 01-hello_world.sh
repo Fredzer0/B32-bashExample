5edcf18377f6c6155b6c94dbf49d53c6c8ffa4b1
Fred <email@Fred.com>
2020-11-19 15:32:53 -0500

    echo-01 termine

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+# ceci est un commentaire
+
+echo "salut antoine"
+# ou
+echo salut antoine
+# ou
+echo 'salut antoine'
+
+echo -n "vive "
+echo "Linux"
+# va ecrire vive linux sans saut de ligne
+
+echo -e "1/n2/n3/n4" #-e interprète les métacaractères
+
+read -p "my name is?" nom #read = cin, -p "mon message" >> variable
+
+echo "tchic ka thcick la tchic" $nom 
+
