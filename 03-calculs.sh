d228c302776504ecd357a6024be9ce5d96f01e16
Fred <email@Fred.com>
2020-11-19 16:31:15 -0500

    03 wowie

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+echo 10+10 #affiche 10+10
+echo $((10+10))
+let resultat=10*10 #si on écrit let devant la variable q'on assigne,
+		   #agit comme un $(())
+echo $resultat
+toto=10*10
+echo $toto
+
+echo "le résultat de $1 - $2 = $(($1-$2)) "
